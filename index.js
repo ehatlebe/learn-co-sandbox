1b9f01ab0baedf0a061d65001a1e3a8f3a5823a6
Ellen H <ellen.hatleberg@gmail.com>
2018-11-05 02:04:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var word = "bird"
+
+console.log(word)
+
+word = "dog"
+
+console.log(word)
+
+/* window.word */
+
+var a = 5
+var b = 2
+var c = 3
+var d = 'hello'
+var e = 'goodbye'
+
+/*
+var a = 5,
+    b = 2,
+    c = 3,
+    d = 'hello',
+    e = 'goodbye';
+
+var a = 5,
+    b = 2,
+    c = 3,
+    d = 'hello',
+    e = 'goodbye';
+*/
+
+console.log(`a: ${a}`)
+console.log(`b: ${b}`)
+console.log(`c: ${c}`)
+console.log(`d: ${d}`)
+console.log(`e: ${e}`)

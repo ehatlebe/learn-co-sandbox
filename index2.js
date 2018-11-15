bfa68d917003f3abd550023a1b99d6d3b97a15e0
Ellen Hatleberg <ellen.hatleberg@gmail.com>
2018-11-15 20:55:10 +0000

    Done.

---
+++ index2.js
@@ -0,0 +1,31 @@
+function sayHello() {
+  console.log('Hello!')
+}
+
+function sayHelloToIsabel() {
+  console.log("Hello, Isabel!")
+}
+
+function sayHelloToSofia() {
+  console.log("Hello, Sofia!")
+}
+
+function sayHelloToBrendan() {
+  console.log("Hello, Brendan!")
+}
+
+sayHello()
+sayHelloToIsabel()
+sayHelloToSofia()
+sayHelloToBrendan()
+
+function doSomething(thing) {
+  console.log(thing)
+}
+ 
+doSomething('anything')
+
+sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`)
+}
+ 
